50349c25b914f7f0f6760a6eaed74f96f2d058c8
Al-Mumeetu Saikat <113462809+almumeetu@users.noreply.github.com>
2024-04-22 14:57:57 +0600

    learn php variable

--- class_04/index.php
+++ class_04/index.php
@@ -1,14 +1,9 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <h1>Hello PHP</h1>
-    <?php
-        echo"Welcome To PHP";
-     ?>
-</body>
-</html>
+<?php 
+
+$name = "Saikat";
+
+
+
+
+
+?>
