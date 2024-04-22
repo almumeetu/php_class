7e5e81b0eaedbeb5f8775ac746a77400e7bcad42
Al-Mumeetu Saikat <113462809+almumeetu@users.noreply.github.com>
2024-04-22 14:58:53 +0600

    its working

--- class_04/index.php
+++ class_04/index.php
@@ -2,8 +2,12 @@
 
 $name = "Saikat";
 
+$price = 20000;
+
+$address = ' Uttara 11, Dhaka 1206, Bangladesh';
+
+$text = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti officia repellat reiciendis perferendis fuga iure beatae illo nulla sed distinctio, nisi magni velit delectus quasi aperiam natus? Accusamus quidem omnis numquam autem sequi laborum quaerat, quos, vitae veniam nam inventore sapiente tempore placeat minus aspernatur obcaecati incidunt perferendis, facere harum !";
 
 
-
-
+echo $address;
 ?>
