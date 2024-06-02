54c6d645c32453d826d412fe21707376ed1e3e35
Al-Mumeetu Saikat <113462809+almumeetu@users.noreply.github.com>
2024-06-02 23:00:45 +0600

    echo-statement

---
+++ LearnPhpWithW3School/echo-print.php
@@ -0,0 +1,42 @@
+<?php 
+
+/* 
+* With PHP, there are two basic ways to get output: echo and print.
+
+* echo and print are more or less the same. They are both used to output data to the screen.
+
+* The differences are small: echo has no return value while print has a return value of 1 so it can be used in expressions. echo can take multiple parameters (although such usage is rare) while print can take one argument. echo is marginally faster than print.
+
+*/
+
+
+
+
+//Echo Statement:
+/*
+* The echo statements can be used with or without parenthees : echo or echo() .
+*/
+
+echo "<h2>PHP is Fun!</h2>";
+echo "Hello world!<br>";
+echo "I'm about to learn PHP!<br>";
+echo "This ", "string ", "was ", "made ", "with multiple parameters.";
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+?>
