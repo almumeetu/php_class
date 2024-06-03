cd4879ad8354411b18d3a2fa667f65000ff74531
Al-Mumeetu Saikat <113462809+almumeetu@users.noreply.github.com>
2024-06-03 16:16:11 +0600

    PHP strlen()

--- LearnPhpWithW3School/string.php
+++ LearnPhpWithW3School/string.php
@@ -4,7 +4,12 @@ $x = "Jhon";
 echo "Hello $x";
 
 
-echo "<h1> String Length </h1>";
-echo "The PHP strlen() function returns the length of a string."
+echo "<h1> String Length </h1>" . "<br>";
+echo "The PHP strlen() function returns the length of a string." . "<br>";
+
+
+echo "String: " . "Hello World" . "<br>"; 
+
+echo strlen("Hello World !");
 
 ?>
