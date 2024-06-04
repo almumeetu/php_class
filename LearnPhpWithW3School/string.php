8f5cece545761fd6519d979905f0ff4a446a75f9
Al-Mumeetu Saikat <113462809+almumeetu@users.noreply.github.com>
2024-06-04 09:43:15 +0600

    PHP str_word_count()

--- LearnPhpWithW3School/string.php
+++ LearnPhpWithW3School/string.php
@@ -1,8 +1,15 @@
 <?php 
+
+
+//String Lengh
+
 $x = "Jhon";
 
 echo "Hello $x";
 
+//The PHP strlen() function returns the length of a string.
+
+echo "strlen()";
 
 echo "<h1> String Length </h1>" . "<br>";
 echo "The PHP strlen() function returns the length of a string." . "<br>";
@@ -10,6 +17,37 @@ echo "The PHP strlen() function returns the length of a string." . "<br>";
 
 echo "String: " . "Hello World" . "<br>"; 
 
-echo strlen("Hello World !");
+echo strlen("Hello World !" . "<br>");
+
+
+echo("Hello World!" . "<br>");
+
+
+
+//Word Count 
+
+// * The PHP str_word_count() function counts the number of words in a string.
+
+echo "str_word_count()". "<br>";
+
+echo str_word_count("Hello Saikat, How manys word here?!") . "<br>";
+
+echo str_word_count('a b c d e f g h i j k l m n o p q r s t u v w x y z') . " " . 'Word Number';
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 ?>
