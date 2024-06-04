b31931c3b9e4a2c60650fc35af2ada3e907a8256
Al-Mumeetu Saikat <113462809+almumeetu@users.noreply.github.com>
2024-06-04 10:34:19 +0600

    php strpos()

--- LearnPhpWithW3School/string.php
+++ LearnPhpWithW3School/string.php
@@ -36,9 +36,18 @@ echo str_word_count('a b c d e f g h i j k l m n o p q r s t u v w x y z') . " "
 
 
 
+// Search For Text Within a String
+
+echo "Search For Text Within a String". "<br>";
+
+echo "strpos()". "<br>";
+
+// The PHP strpos() function searches for a specific text within a string.
+
+// If a match is found,  the function returns the character position of the first match. If no math is found, it will return False.
 
 
-
+echo strpos("Hello World!", "o");
 
 
 
