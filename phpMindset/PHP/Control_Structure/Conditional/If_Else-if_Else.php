d8e6484bdac6fb084a4ca6a221234567542a57b9
Al-Mumeetu Saikat <almumeetu@gmail.com>
2024-06-30 02:41:22 +0600

    if condition Syntax

--- phpMindset/PHP/Control_Structure/Conditional/If_Else-if_Else.php
+++ phpMindset/PHP/Control_Structure/Conditional/If_Else-if_Else.php
@@ -1,6 +1,11 @@
-
 <?php 
 
-echo "<h1> This is the Condintional Control Structure (If_Else-if_Else)</h1>"
+    echo "<h1> This is the Condintional Control Structure (If_Else-if_Else)</h1>"
 
+    // Control Structures (If / Else if / Else)
+
+    //Syntax:
+    if($Condition){
+        
+    }
 ?> 
